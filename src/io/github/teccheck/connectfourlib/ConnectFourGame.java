0d508081f553cd996511cadf61a68ff00ddb9795
Chris <mcteccy@gmail.com>
2020-06-20 22:02:05 +0200

    Added some comments + cleanup

--- src/io/github/teccheck/connectfourlib/ConnectFourGame.java
+++ src/io/github/teccheck/connectfourlib/ConnectFourGame.java
@@ -2,56 +2,61 @@ package io.github.teccheck.connectfourlib;
 
 public class ConnectFourGame {
 
+	public static final int STATUS_MOVE_FAILED = -1;
+	public static final int STATUS_MOVE_NO_WINNER = 0;
+
 	int[][] table;
 	int requiredLenght;
-	int winner = 0;
 
+	/**
+	 * @param sizeX:          the height of the table
+	 * @param sizeY:          the width of the table
+	 * @param requiredLenght: the length required for a player to win
+	 */
 	public ConnectFourGame(int sizeX, int sizeY, int requiredLenght) {
 		this.table = new int[sizeX][sizeY];
 		this.requiredLenght = requiredLenght;
 	}
 
 	public ConnectFourGame() {
-		this.table = new int[7][6];
-		this.requiredLenght = 4;
-	}
-
-	public int getWinner() {
-		return winner;
+		this(7, 6, 4);
 	}
 
 	public int[][] getTable() {
 		return table;
 	}
 
-	public boolean tryMove(int player, int x) {
+	/**
+	 * @param player: the player that want's to make a move
+	 * @param x:      the row i which the player attempts to put a chip
+	 * @return either STATUS_MOVE_FAILED, STATUS_MOVE_NO_WINNER or the player that
+	 *         has won in that round
+	 */
+	public int tryMove(int player, int x) {
 		if (x < 0 || x >= table.length)
-			return false;
+			return STATUS_MOVE_FAILED;
 
 		// go from bottom to top to find next free place
 		for (int y = table[x].length - 1; y >= 0; y--) {
 			if (table[x][y] == 0) {
 				table[x][y] = player;
-				winner = checkWinner(player, x, y);
-				return true;
+				return checkWinner(player, x, y);
 			}
 		}
-
-		return false; // if no free place was found
+		return STATUS_MOVE_FAILED; // if no free place was found
 	}
 
 	int checkWinner(int player, int x, int y) {
 		// check in all directions
-		if (checkLine(player, x, y, 1, 0) || checkLine(player, x, y, 0, 1) || checkLine(player, x, y, 1, 1)
-				|| checkLine(player, x, y, -1, 1))
+		if (checkForLine(player, x, y, 1, 0) || checkForLine(player, x, y, 0, 1) || checkForLine(player, x, y, 1, 1)
+				|| checkForLine(player, x, y, -1, 1))
 			return player;
 
 		return 0;
 	}
 
-	public boolean checkLine(int player, int x, int y, int dirX, int dirY) {
-		int lenght = 0;
-		for (int i = -(requiredLenght - 1); i < requiredLenght; i++) {
+	boolean checkForLine(int player, int x, int y, int dirX, int dirY) {
+		for (int lenght = 0, i = -(requiredLenght - 1); i < requiredLenght; i++) {
 			if (hasChip(player, x + dirX * i, y + dirY * i)) {
 				lenght++;
 				if (lenght >= requiredLenght)
@@ -65,7 +70,6 @@ public class ConnectFourGame {
 	boolean hasChip(int player, int x, int y) {
 		if (x < 0 || y < 0 || x >= table.length || y >= table[x].length)
 			return false;
-
 		return table[x][y] == player;
 	}
 }
